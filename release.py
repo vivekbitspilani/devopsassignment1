5f6712728c3f1907deab733bff5b5a86411cfbb5
vivekbitdurg <vivek.sinha1105@gmail.com>
2021-04-10 21:39:10 +0530

    reset changes

--- release.py
+++ release.py
@@ -2,5 +2,9 @@ print("This will create first release")
 
 print("Bugs modified in release version 1.1")
 
-
 print("will now try to create problem so the I can force push")
+
+print("just adding another line for ammend ")
+
+
+print("adding another line for reset")
